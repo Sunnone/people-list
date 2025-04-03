07f27e2f6da8531fec5943d0e0bb6b8c12212f13
Sunnone <vanyayes@gmail.com>
2025-04-03 12:43:31 +0600

    1

--- src/main/java/org/example/PersonDAO.java
+++ src/main/java/org/example/PersonDAO.java
@@ -1,27 +1,37 @@
 package org.example;
 
+import jakarta.transaction.Transactional;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public class PersonDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
 
+    @Transactional
     public void addPerson(Person person) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.save(person);
             transaction.commit();
         }
     }
 
+    @Transactional
     public List<Person> getAllPeople() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery("from Person", Person.class).list();
         }
     }
 
     public void updatePerson(Person person){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+        try(Session session = sessionFactory.openSession()){
             Transaction transaction = session.beginTransaction();
             session.update(person);
             transaction.commit();
@@ -29,7 +39,7 @@ public class PersonDAO {
     }
 
     public void deletePerson(Person person){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+        try(Session session = sessionFactory.openSession()){
             Transaction transaction = session.beginTransaction();
             session.delete(person);
             transaction.commit();
@@ -37,7 +47,7 @@ public class PersonDAO {
     }
 
     public Person getById(Long id){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery("from Person where id = :id", Person.class)
                     .setParameter("id", id)
                     .uniqueResult();
